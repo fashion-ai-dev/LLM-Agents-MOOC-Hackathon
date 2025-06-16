6304df9ad4d7a4eea0eb1987c070766742cbc966
fashion-ai-dev <leo@generativecrm.com>
2025-06-16 14:11:31 -0400

    working version

---
+++ backend/style_agent/style_input_tool.py
@@ -0,0 +1,11 @@
+async def fashion_input(text,sid):
+    objeto=""
+
+    #step 1: quebrar o TEXT como o strategy factory
+    #Criar embeddings
+    #Objeto contem os textos processados e também a lista de embeddings
+
+
+
+
+    return objeto
